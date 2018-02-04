044b1feff4de50f2ed3458cf4522c5eb0811533f
Artem Pohaliyuk <Hisus89@mail.ru>
2018-02-04 21:06:41 +0200

    add last pages

--- laser/js/common.js
+++ laser/js/common.js
@@ -24,6 +24,16 @@ $( document ).ready( function() {
 
   var hoverLine = $( '.hover-line' );
 
+  $( '.nav__list > .nav__item' ).each( function() {
+    var offsetLeft = $( this ).offset().left;
+    var linkWidth = $( this ).width();
+    var alignCenter = offsetLeft + ( linkWidth / 2 );
+
+    if ( $( this ).hasClass( 'active' ) ) {
+      $( hoverLine ).fadeIn().width( alignCenter );
+    }
+  });
+
   $( '.nav__list > .nav__item' )
     .mouseenter( function( e ) {
       var offsetLeft = $( this ).offset().left;
@@ -31,21 +41,30 @@ $( document ).ready( function() {
       var alignCenter = offsetLeft + ( linkWidth / 2 );
 
       if ( $( this ).hasClass( 'dropdown' ) ) {
-        $( hoverLine ).fadeIn().width( alignCenter - 15 );
+        $( hoverLine ).fadeIn().width( alignCenter - 10 );
       } else {
         $( hoverLine ).fadeIn().width( alignCenter );
       }
     });
 
   $( '.nav__list' ).mouseleave( function() {
-    $( hoverLine ).fadeOut( 0 );
+    if ( $( '.nav__item' ).hasClass( 'active' ) ) {
+      var active = $( '.nav__item.active' );
+      var offsetLeft = $( active ).offset().left;
+      var linkWidth = $( active ).width();
+      var alignCenter = offsetLeft + ( linkWidth / 2 );
+
+      $( hoverLine ).fadeIn().width( alignCenter - 10 );
+    } else {
+      $( hoverLine ).fadeOut( 0 );
+    }
   });
 
   $( '.slider' ).each( function() {
     if ( $( this ).hasClass( 'comments__slider' ) ) {
       $( this ).slick({
-        appendDots: $( '.comments__nav' ),
-        appendArrows: $( '.comments__nav' )
+        appendDots: $( this ).closest( '.slider-wrap' ).find( '.custom-nav' ),
+        appendArrows: $( this ).closest( '.slider-wrap' ).find( '.custom-nav' )
       });
     } else {
       $( this ).slick();
@@ -106,27 +125,6 @@ $( document ).ready( function() {
       scrollTop: 0
     }, 1000 );
   });
-
-  // $( '.js-collapse' ).click( function() {
-  //   if ( $( window ).width() <= 991 ) {
-  //     $( this ).siblings( '.collapse-content' ).slideToggle();
-  //   }
-  // });
-
-  // $( '.js-play-video' ).click( function() {
-  //
-  //   var url = $( this ).find( 'a' ).attr( 'href' );
-  //   var matches = /\/\/(?:www\.)youtube\.com\/.*(?:\?|&)v=([^&]*)/i.exec( url );
-  //
-  //   if ( !matches ) {
-  //     matches = /\/\/(?:www\.)youtu\.be\/([^\?&]*)/i.exec( url );
-  //   }
-  //
-  //   if ( matches ) {
-  //     var videoId = matches[ 1 ];
-  //     $( this ).replaceWith( $( '<iframe>' ).prop( 'src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1' ) );
-  //   }
-  // });
 
   $( 'form' ).each( function() {
     var it = $( this );
@@ -216,5 +214,25 @@ $( document ).ready( function() {
       });
     });
   }
+
+  $( '.js-show-category-menu' ).click( function() {
+    $( this ).toggleClass( 'open' );
+    $( '.category-menu' ).slideToggle();
+  });
+
+  $( '.js-play-video' ).click( function () {
+
+    var url = $( this ).find( 'a' ).attr( 'href' );
+    var matches = /\/\/(?:www\.)youtube\.com\/.*(?:\?|&)v=([^&]*)/i.exec( url );
+
+    if ( !matches ) {
+      matches = /\/\/(?:www\.)youtu\.be\/([^\?&]*)/i.exec( url );
+    }
+
+    if ( matches ) {
+      var videoId = matches[ 1 ];
+      $( this ).replaceWith( $( '<iframe>' ).prop( 'src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1' ) );
+    }
+  });
 
 });
